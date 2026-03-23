32c246fceaef9c0faa1bbcfa59766b7bb7f37455
Benedikt Spranger <b.spranger@linutronix.de>
2026-03-23 20:40:30 +0100

    More cryptolib stuff
    Signed-off-by: Benedikt Spranger <b.spranger@linutronix.de>

--- firmware/application/src/main.c
+++ firmware/application/src/main.c
@@ -28,13 +28,21 @@ int main(void)
 		.tm_sec = 0,
 	};
 
-	uint8_t random_number[32];
+	uint8_t random_number[32], sn[ATCA_SERIAL_NUM_SIZE];
 	ATCA_STATUS status;
-	int ret;
+	bool is_locked;
 
 	printk("Eurovibes Sertest-NG Application %s\n", APP_VERSION_STRING);
 	LOG_INIT();
 	log_thread_set(k_current_get());
+
+	if (usb_init()) {
+		LOG_ERR("Failed to initialize USB device");
+		return -ENODEV;
+	}
+
+	LOG_INF("USB device support enabled");
+	k_sleep(K_MSEC(5000));
 
 	if (get_type_plate()) {
 		LOG_ERR("EEPROM device not ready");
@@ -49,8 +57,40 @@ int main(void)
 		return -ENODEV;
 	}
 
-	atcab_random(&random_number);
-	LOG_INF("Random: 0x%08x", *((uint32_t *) random_number));
+	status = atcab_is_locked(LOCK_ZONE_CONFIG, &is_locked);
+	if (status != ATCA_SUCCESS)
+	{
+		LOG_ERR("failed to get lock status: %02x", status);
+		//return -ENODEV;
+	} else {
+		LOG_INF("ATECC608 is %s", (is_locked) ? "locked" : "unlocked");
+	}
+
+	status = atcab_read_serial_number(sn);
+	if (status != ATCA_SUCCESS)
+	{
+		LOG_ERR("failed to get serial number: %02x", status);
+		//return -ENODEV;
+	} else {
+		uint32_t *d = (uint32_t *) sn;
+		int i;
+
+		for (i = 0; i < 8; i++)
+			LOG_INF("Serial %d: 0x%08x", i, d[i]);
+	}
+
+	status = atcab_random(random_number);
+	if (status != ATCA_SUCCESS)
+	{
+		LOG_ERR("failed to get random number: %02x", status);
+		//return -ENODEV;
+	} else {
+		uint32_t *d = (uint32_t *) random_number;
+		int i;
+
+		for (i = 0; i < 8; i++)
+			LOG_INF("Random %d: 0x%08x", i, d[i]);
+	}
 
 	if (rtc_init()) {
 		LOG_ERR("RTC device is not ready");
@@ -59,16 +99,11 @@ int main(void)
 
 	LOG_INF("RTC device support enabled");
 
-	if (usb_init()) {
-		LOG_ERR("Failed to initialize USB device");
-		return -ENODEV;
-	}
-
-	LOG_INF("USB device support enabled");
-
 	set_date_time(&tm);
 
 	while (1) {
+		int ret;
+
 		ret = get_date_time(&tm);
 		if (ret)
 			LOG_ERR("get_date_time() failed: %d", ret);
